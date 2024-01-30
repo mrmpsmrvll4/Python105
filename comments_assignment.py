3c4374da627ec8fa17e95b35c66c23d91e7462e9
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-01-30 11:53:54 -0600

    comments

---
+++ comments_assignment.py
@@ -0,0 +1,9 @@
+#tank is 15 gallons
+#the distance is 600 miles
+#divide the distance by the tank size
+#the goal is to find miles per gallon
+
+tank_size = 15
+distance = 600
+mpg = distance / tank_size
+print("Miles per gallon:", mpg)
