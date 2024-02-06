ad5ba32f9fdc171ca65a810528e69afbe2941609
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-02-06 11:04:13 -0600

    decisons

---
+++ 1/decisions_assignment.py
@@ -0,0 +1,19 @@
+#What is your age and how does it affect you with laws?
+
+age = input("How old are you?")
+
+#check if age is 16 or older
+if age >= "16":
+    print("You are old enough to drive")
+
+#check if age is 18 or older
+if age >= "18":
+    print("You can vote")
+
+#check if age is 21 or older
+if age >= "21":
+    print("You can buy alcohol legally")
+
+#check if age is less than 65
+if age < "65":
+    print("You are not eligible for a senior discount")
