9918be310e6ce67b86a2dd45e3891d75f5ab4eb5
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-02-08 11:11:19 -0600

    finish logical operations

---
+++ logical_operations_assignment.py
@@ -0,0 +1,44 @@
+#This code demonstrates the use of logical operators in Python
+
+#enter a couple numbers
+a = int(input("Enter an integer"))
+b = int(input("Enter another integer"))
+
+
+# and operator 1
+if a > b and a > 5
+    print("a is greater than 5")
+elif b > a and b > 5
+    print("b is greater than 5")
+    
+# and operator 2
+if b > a and b > 10
+    print("b is greater than 10")
+elif a > b and a > 10
+    print("a is greater than 10")
+    
+
+# or operator 1
+if if a > b or a < 5
+    print("")
+elif b > a or b < 5
+    print("")
+
+# or operator 2
+if if b > a or b < 10
+    print("")
+elif a > b or a < 10
+    print("")
+
+
+# not operator 1
+if if a > b not
+    print("")
+elif b > a not 
+    print("")
+
+# not operator 2
+if if b > a not 
+    print("")
+elif a > b not 
+    print("")
