328745d38f2bfa036bb079b175d296a1934dee1a
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-02-08 10:07:43 -0600

    updated decisions assignment

--- 1/decisions_assignment.py
+++ 1/decisions_assignment.py
@@ -5,16 +5,23 @@ age = input("How old are you?")
 #check if age is 16 or older
 if age >= "16":
     print("You are old enough to drive")
+else:
+    print("You are not old enough to drive")
 
 #check if age is 18 or older
 if age >= "18":
     print("You can vote")
+else:
+    print("You cannot vote")
 
 #check if age is 21 or older
 if age >= "21":
     print("You can buy alcohol legally")
+else:
+    print("You cannot buy alcohol legally")
 
 #check if age is less than 65
 if age < "65":
     print("You are not eligible for a senior discount")
-    
+else:
+    print("You are eligible for a senior discount")
