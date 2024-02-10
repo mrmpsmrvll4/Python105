1bfbeb8bea36ece16ff9c4048d407b64589396d9
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-02-10 13:26:10 -0600

    finished assignment

---
+++ assignments/divisible_by_7.py
@@ -0,0 +1,6 @@
+# Iterate through the numbers from 1 to 300
+for num in range(1, 301):
+    # Check if the number is divisible by 7
+    if num % 7 == 0:
+        # If divisible by 7, print the number
+        print(num)
