d442a40780153204774c986dc32a53a41bfba849
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-02-21 15:12:53 -0600

    finished functions hw

---
+++ assignments/functions_area.py
@@ -0,0 +1,13 @@
+# area of a circle
+def circle(radius):
+    area = 3.14 * radius * radius
+    print("The area of the circle is " + str(area) + " square units")
+    
+# area of a square
+def square(side):
+    area = side * side
+    print("The area of the square is " + str(area) + " square units")
+
+# test
+circle(8)
+square(7)
