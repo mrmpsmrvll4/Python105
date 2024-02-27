d903fa558d7a72f048beacc77c75c93317561ae0
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-02-26 21:31:09 -0600

    i am confuzzled

--- assignments/return_function.py
+++ assignments/return_function.py
@@ -1,12 +1,14 @@
 # calculate the interest
 
 def calculate_interest(Principal_Amount, Interest_Rate, Investment_Time):
-    result = Principal_Amount * Interest_Rate * Investment_Time /100
-    print(result)
+    total = (Principal_Amount * Interest_Rate * Investment_Time / 100)
+    return total
 
-result = calculate_interest(1000, 5, 2)
+result = calculate_interest()
 print(f"The simple interest for a principal amount of ${Principal_Amount:,.2f} \
-        at an interest rate of {Interest_Rate}% over a period of \
-            {Investment_Time} years is: ${calculate_interest:,.2f}")
+                at an interest rate of {Interest_Rate}% over a period of \
+                {Investment_Time} years is: ${calculate_interest:,.2f}")
+
+calculate_interest(1000, 5, 2)
 
 #fix so that an answer shows up
