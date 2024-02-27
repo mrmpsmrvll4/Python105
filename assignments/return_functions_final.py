01bfe623c9b73216478a330983a60ddde872e628
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-02-26 21:52:06 -0600

    continuing to work on it

---
+++ assignments/return_functions_final.py
@@ -0,0 +1,18 @@
+# calculate the interest
+
+print = int(input("Enter the Principal Amount":  ))
+print = int(input("Enter the Interest Rate as a whole number:  "))
+print = int(input("Enter the Investment Time in whole years:  "))
+
+def calculate_interest(Principal_Amount, Interest_Rate, Investment_Time):
+    total = (Principal_Amount * Interest_Rate * Investment_Time / 100)
+    return total
+
+result = calculate_interest()
+print(f"The simple interest for a principal amount of ${Principal_Amount:,.2f} \
+                at an interest rate of {Interest_Rate}% over a period of \
+                {Investment_Time} years is: ${calculate_interest:,.2f}")
+
+calculate_interest(1000, 5, 2)
+
+#fix so that an answer shows up
