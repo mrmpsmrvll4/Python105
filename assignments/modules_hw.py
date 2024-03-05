76858cf4c4c08fb0f420e6f87f451028eb57b853
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-03-05 11:43:30 -0600

    finshed classwork

--- assignments/modules_hw.py
+++ assignments/modules_hw.py
@@ -1,24 +1,31 @@
 import random
 
-# create a random number guessing game
+# create a number guessing game program with 
 
 def game(win):
     try:
         guess = int (input("Guess a number between 1 and 100:  "))
-
         # print value hot, etc.
-
-        if:
-            print()
+        distance = abs(win - guess)
+        if distance == 0:
+            print("You win!")
+        elif distance <= 5:
+            print("Very hot")
+        elif distance <= 15:
+            print("Hot")
+        elif distance <= 25:
+            print("Cool")
         else:
-            print("That is not a valid value")
-            game(win)
-
+            print("Cold")
+        game(win)
     except:
-        print()
+        print("That is not a valid value")
         game(win)
 
 def main():
+    # test random
     win = random.randint(1,100)
     print(win)
     game(win)
+
+main()
