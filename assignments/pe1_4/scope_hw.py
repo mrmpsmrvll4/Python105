07e10d2eeaf29af1d020bc081cf339e3f5c98fa2
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-03-06 21:05:58 -0600

    reorganized hw into folders

--- assignments/pe1_4/scope_hw.py
+++ assignments/pe1_4/scope_hw.py
@@ -10,14 +10,14 @@ def calculate_bmi(weight, height):
     calculate_bmi = weight (KG) / (height (M) * height (M))
 
 def main():
-    for
-input int(f"Please enter your {weight} in pounds:  ")
+    for i in weight:
+        input int(f"Please enter your {weight} in pounds:  ")
     
     
-input int(f"Please enter your {height} in inches:  ")
+        input int(f"Please enter your {height} in inches:  ")
 
     
-print(f"Your BMI is{()} ")
+        print(f"Your BMI is{()} ")
 
 
 
