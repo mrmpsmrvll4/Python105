ff9ad764e1fa8f2cdffa89b7f19e1aae7f33076a
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-03-06 19:35:12 -0600

    updated exceptions hw

--- assignments/exceptions_hw.py
+++ assignments/exceptions_hw.py
@@ -1,18 +1,18 @@
 # Simple Python program to calculate the square of a number
 
 def square_number():
-    number = input("Enter a number to square: ")
-    squared_number = int(number) ** 2
-    print(f"The square of {number} is {squared_number}.")
-
     try:
+        number = input("Enter a number to square: ")
+        squared_number = int(number) ** 2
+        print(f"The square of {number} is {squared_number}.")
 
     except ValueError:
-        print()
+        print("That is not a number. Please enter a number.")
 
     else:
-        print()
+        print("Thank you for using the square program.")
+    
     finally:
-        print()
+        print("Program has ended")
 
 square_number()
