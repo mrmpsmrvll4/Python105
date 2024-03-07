78ad6feba47797517a766ad95ff879e4c1180dd9
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-03-06 20:49:08 -0600

    reorganizing notes and eventually the assignments

--- assignments/scope_hw.py
+++ assignments/scope_hw.py
@@ -1,2 +1,24 @@
 # createa a bmi calculator using scope
 
+global KG
+KG = 0.453592
+
+global M
+M = 0.0254
+
+def calculate_bmi(weight, height):
+    calculate_bmi = weight (KG) / (height (M) * height (M))
+
+def main():
+    for
+input int(f"Please enter your {weight} in pounds:  ")
+    
+    
+input int(f"Please enter your {height} in inches:  ")
+
+    
+print(f"Your BMI is{()} ")
+
+
+
+main()
