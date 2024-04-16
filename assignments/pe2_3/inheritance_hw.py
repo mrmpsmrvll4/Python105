038045a12af8612820e53e29a374e18fabebd867
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-04-15 23:30:55 -0500

    dont understand inheritance hw

--- assignments/pe2_3/inheritance_hw.py
+++ assignments/pe2_3/inheritance_hw.py
@@ -23,3 +23,44 @@ Part 2: Write a script to:
     - Store and then display the data using the object's methods.
 
 """
+# make a class
+
+class Employee:
+
+    def __init__(self, employee_name, employee_number):
+       
+        self.__employee_name = employee_name
+        self.__employee_number = employee_number
+
+    # set getters
+    def get_employee_name(self):
+        return self.__employee_name
+    
+    def get_employee_number(self):
+        return self.__employee_number
+    
+    # set setters
+    def set_employee_name(self, value):
+        self.__employee_name = value
+    
+    def set_employee_number(self, value):
+        self.__employee_number = value
+
+# make a subclass
+class Productionworker:
+
+    # added shift number and hourly pay rate to class
+    def __init__(self, employee_name, employee_number, shift_number, hourly_pay_rate):
+
+        super().__init__(self, employee_name, employee_number)
+        self.shift_number = shift_number
+        self.hourly_pay_rate = hourly_pay_rate
+
+# make function to have production worker and ask for info and store and display data
+def main():
+    print
+
+
+
+# call the main function
+main()      
