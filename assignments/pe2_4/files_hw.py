2a67e71f7d6d226a102d727af71bc6c09d8e5203
mrmpsmrvll4 <msomerville9256@students.mchenry.edu>
2024-04-18 10:38:34 -0500

    thursday

---
+++ assignments/pe2_4/files_hw.py
@@ -0,0 +1,19 @@
+"""
+
+Objective: Create a Python program that functions as a personal diary. Users should be able to add entries with the date and time they provide, which are then saved to a file.
+----------
+Detailed Directions
+1. Set Up Your Python Environment: Create a diary folder for this assignment, you will upload the whole file to GitHub when you are done. 
+2. Create a New Python File: Name this file personal_diary.py.
+3. Writing the Code:
+    - Create this in a main function
+    - Prompt the user to enter the current date and time, then a diary entry.
+    - Open or create a file named diary.txt in append mode using open().
+    - Write the user-provided date, time, and diary entry into the file. 
+      Ensure each entry is separated from the others by writing a blank line after entering the date/time line and the entry line. 
+    - Close the file using the close() method.
+4. Comments and Documentation: Add comments to your code explaining its functionality.
+5. Testing the Program: Run personal_diary.py three times, each time entering a different diary entry along with the date and time. Check diary.txt to ensure entries are recorded correctly.
+6. Submission: Submit both your personal_diary.py file and the diary.txt file containing your entries. 
+
+"""
---
+++ notes/pe2_4/files.py
@@ -0,0 +1,10 @@
+"""
+
+track mileage for work
+calculate reimbursement based on mileage rate
+store in external file
+
+"""
+
+
+milage_rate = 
